d6adfa2eda97c94068aac5647f8fe302c9db3394
IsaStev <isabela.alves08.fatec@gmail.com>
2025-03-04 16:47:12 -0300

    Update Ex28_MediaVendas.java

--- Ex28_MediaVendas.java
+++ Ex28_MediaVendas.java
@@ -14,15 +14,19 @@ public class Ex28_MediaVendas {
         if (mediaVendas < 500 && precoAtual < 30) {
             novoPreco = precoAtual + (precoAtual * 0.10); //aumenta 10%
         }
-        else if (mediaVendas >= 500 && mediaVendas < 1000 && precoAtual >=30 && precoAtual <80) {
-                novoPreco = precoAtual + (precoAtual * 0.15); // aumenta 15%
-        }
-            else if (mediaVendas >= 1000 && precoAtual >= 80) {
-                    novoPreco = precoAtual - (precoAtual * 0.05); // treduz 5%
+        else{
+            if (mediaVendas >= 500 && mediaVendas < 1000 && precoAtual >=30 && precoAtual <80) {
+                novoPreco = precoAtual + (precoAtual * 0.15); // aumenta 15%    
             }
+            else{ 
+                if (mediaVendas >= 1000 && precoAtual >= 80) {
+                    novoPreco = precoAtual - (precoAtual * 0.05); // treduz 5%
+                }
                 else{
                     novoPreco = precoAtual;
                 } 
-    JOptionPane.showMessageDialog (null, ("O novo preço do produto deve ser: " + novoPreco));
+            }
+        }
+        JOptionPane.showMessageDialog (null, ("O novo preço do produto deve ser: " + novoPreco));
     }
 }
