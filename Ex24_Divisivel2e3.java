5f018ef54d892eeb1a603cd52d373c1af75c7da8
IsaStev <isabela.alves08.fatec@gmail.com>
2025-03-04 16:50:44 -0300

    Update Ex24_Divisivel2e3.java

--- Ex24_Divisivel2e3.java
+++ Ex24_Divisivel2e3.java
@@ -1,4 +1,4 @@
-/**
+ /**
  *Receba um número e verifique se é divisivel por 2 e 3
  * @author Isabela PC
  */
@@ -13,14 +13,18 @@ public class Ex24_Divisivel2e3 {
         if (n1 % 2 == 0 && n1 % 3 == 0) {
             JOptionPane.showMessageDialog(null, ("O número é divisível por 2 e 3."));
         }
-        else if (n1 % 2 == 0) {
+        else {
+            if (n1 % 2 == 0) {
                 JOptionPane.showMessageDialog(null, ("O número é divisível apenas por 2."));
-        }
-            else if (n1 % 3 == 0) {
-                    JOptionPane.showMessageDialog(null, ("O número é divisível apenas por 3."));
             }
+            else {
+                if (n1 % 3 == 0) {
+                    JOptionPane.showMessageDialog(null, ("O número é divisível apenas por 3."));
+                }
                 else{
                     JOptionPane.showMessageDialog(null, ("O número não é divisível nem por 2 nem por 3."));
                 }
+            }
+        }
     }
 }
